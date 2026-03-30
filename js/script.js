05c16d11e0ce85b3237994307adb71d6c807c574
Bohdan Olefirenko <bohdan.olefirenko2k4@gmail.com>
2026-03-29 22:52:16 -0400

    Fetch the data and selected DOM elements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 console.log("script.js loaded");
 
 const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=ofjHD4j78FCEF3kDRU6g4MqnZLIr3bfo&q=Dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+
+async function fetchGifs() {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  console.log(data); // inspect structure
+
+  const images = data.data.map(gif => gif.images.original.url);
+
+  return images;
+}
+
+const gifContainer = document.querySelector("#gif-container");
+const button = document.querySelector("#fetch-gif-btn");
