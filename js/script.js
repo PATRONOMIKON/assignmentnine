cdf534424541fa8bdeafceeae138f5cf07d5a677
Bohdan Olefirenko <bohdan.olefirenko2k4@gmail.com>
2026-03-29 22:48:34 -0400

    Created API key and generated a request

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 console.log("script.js loaded");
+
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=ofjHD4j78FCEF3kDRU6g4MqnZLIr3bfo&q=Dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
