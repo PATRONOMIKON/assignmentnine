0a68d9905ddac8493da53fe5e1d92be83836e625
Bohdan Olefirenko <bohdan.olefirenko2k4@gmail.com>
2026-03-29 23:06:11 -0400

    Added the Event Listener, tested the hardcoded version of the site then completed the extra creadit challenge

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
-console.log("script.js loaded");
-
-const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=ofjHD4j78FCEF3kDRU6g4MqnZLIr3bfo&q=Dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
-
-async function fetchGifs() {
-  const response = await fetch(endpoint);
-  const data = await response.json();
-
-  console.log(data); // inspect structure
-
-  const images = data.data.map(gif => gif.images.original.url);
-
-  return images;
-}
-
 const gifContainer = document.querySelector("#gif-container");
 const button = document.querySelector("#fetch-gif-btn");
+const input = document.querySelector("#search-input");
+
+const API_KEY = "ofjHD4j78FCEF3kDRU6g4MqnZLIr3bfo";
+
+async function fetchGifs(searchTerm) {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=25&rating=g` ;
+
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    const images = data.data.map(gif => gif.images.original.url);
+
+    return images;
+  } catch (error) {
+    console.error("Error fetching gifs:", error);
+    return [];
+  }
+}
+
+button.addEventListener("click", async () => {
+  const searchTerm = input.value || "Dogs";
+
+  gifContainer.innerHTML = "";
+
+  const images = await fetchGifs(searchTerm);
+
+  images.forEach(url => {
+    gifContainer.innerHTML += `
+      <img src="${url}" class="col-3 mb-3">
+    `;
+  });
+});
